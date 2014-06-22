debc40f1c4869cfa534072f7744b080aaa978f54
Michael MacInnis <michael.p.macinnis@gmail.com>
2014-06-22 15:31:35 -0400

    Move variable and constant to ref.go.

--- cell.go
+++ cell.go
@@ -78,14 +78,6 @@ type Context interface {
 	PublicSyntax(k string, f Function)
 }
 
-type Reference interface {
-	Cell
-
-	Copy() Reference
-	Get() Cell
-	Set(c Cell)
-}
-
 const (
 	SaveCarCode = 1 << iota
 	SaveCdrCode
@@ -2693,56 +2685,3 @@ func (u *Unbound) Self() Context {
 	return nil
 }
 
-/* Variable cell definition. */
-
-type Variable struct {
-	v Cell
-}
-
-func NewVariable(v Cell) Reference {
-	return &Variable{v}
-}
-
-func (vr *Variable) Bool() bool {
-	return true
-}
-
-func (vr *Variable) String() string {
-	return fmt.Sprintf("%%variable %p%%", vr)
-}
-
-func (vr *Variable) Equal(c Cell) bool {
-	return vr.v.Equal(c)
-}
-
-/* Variable-specific functions */
-
-func (vr *Variable) Copy() Reference {
-	return NewVariable(vr.v)
-}
-
-func (vr *Variable) Get() Cell {
-	return vr.v
-}
-
-func (vr *Variable) Set(c Cell) {
-	vr.v = c
-}
-
-/* Constant cell definition. */
-
-type Constant struct {
-	Variable
-}
-
-func NewConstant(v Cell) *Constant {
-	return &Constant{Variable{v}}
-}
-
-func (ct *Constant) String() string {
-	return fmt.Sprintf("%%ct %p%%", ct)
-}
-
-func (ct *Constant) Set(c Cell) {
-	panic("ct cannot be set")
-}
