b0929e43cc0e83f024c274dbd3fd6b72edd2fb5f
Michael MacInnis <michael.p.macinnis@gmail.com>
2016-08-28 01:20:59 -0400

    Formatting.

--- pkg/task/deref.go
+++ pkg/task/deref.go
@@ -9,26 +9,27 @@ import (
 
 func Deref(name string, address uintptr) Cell {
 	switch {
+	case name == "channel":
+		return (*Channel)(unsafe.Pointer(address))
+	case name == "pipe":
+		return (*Pipe)(unsafe.Pointer(address))
+	case name == "task":
+		return (*Task)(unsafe.Pointer(address))
+
 	case name == "bound":
 		return (*bound)(unsafe.Pointer(address))
 	case name == "builtin":
 		return (*builtin)(unsafe.Pointer(address))
-	case name == "channel":
-		return (*Channel)(unsafe.Pointer(address))
 	case name == "continuation":
 		return (*continuation)(unsafe.Pointer(address))
 	case name == "method":
 		return (*method)(unsafe.Pointer(address))
 	case name == "object":
 		return (*object)(unsafe.Pointer(address))
-	case name == "pipe":
-		return (*Pipe)(unsafe.Pointer(address))
 	case name == "scope":
 		return (*scope)(unsafe.Pointer(address))
 	case name == "syntax":
 		return (*syntax)(unsafe.Pointer(address))
-	case name == "task":
-		return (*Task)(unsafe.Pointer(address))
 	case name == "unbound":
 		return (*unbound)(unsafe.Pointer(address))
 	}
