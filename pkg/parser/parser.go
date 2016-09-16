94304a8846f96d8ca4456b98d99bde4e8ed89799
Michael MacInnis <michael.p.macinnis@gmail.com>
2016-09-15 22:21:24 -0400

    Switch to goyacc as go too yacc is being removed.

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -365,7 +365,7 @@ func (p *parser) Parse(
 	return rval == 0
 }
 
-//go:generate go tool yacc -o grammar.go grammar.y
+//go:generate goyacc -o grammar.go grammar.y
 //go:generate sed -i.save -f grammar.sed grammar.go
 //go:generate go fmt grammar.go
 //go:generate rm -f y.output grammar.go.save
